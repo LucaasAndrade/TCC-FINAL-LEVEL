0f28dba238dd4bc5094f907616eadd1a1a01e6ab
Rayssa Rodrigues <rayssa.rodrigues1198@gmail.com>
2022-09-23 02:16:48 -0300

    telas f detalhes

--- site/src/pages/admin/cadastrarproduto/index.js
+++ site/src/pages/admin/cadastrarproduto/index.js
@@ -1,109 +1,138 @@
-import './index.scss'
-
-
-
+import "./index.scss";
 
 export default function Cadastrarproduto() {
+  return (
+    <main className="page-cadastro">
+      <header>
+        <div className="faixa-header">
+          <img src="/images/logo.png" alt="logo"></img>
+          <div className="topicos">
+            <p>ADICIONAR PRODUTO</p>
+            <p className="p-topicos">LISTAR PEDIDOS</p>
+            <p className="p-topicos">LISTAR PRODUTOS</p>
+            <p className="p-topicos">CHAT</p>
+            <p className="p-topicos">CUPONS</p>
+            <p className="p-topicos">HISTÓRICO</p>
+          </div>
+          <div>
+            <button className="botao-painel">PAINEL</button>
+          </div>
+        </div>
+      </header>
 
-    return (
-        <main className='page-cadastro'>
-
-            <header>
-
-                <div className='faixa-header'>
-                    <img src='/images/logo.png' alt='logo'></img>
-                    <div className='topicos'>
-                        <p >ADICIONAR PRODUTO</p>
-                        <p className='p-topicos'>LISTAR PEDIDOS</p>
-                        <p className='p-topicos'>LISTAR PRODUTOS</p>
-                        <p className='p-topicos'>CHAT</p>
-                        <p className='p-topicos'>CUPONS</p>
-                        <p className='p-topicos'>HISTÓRICO</p>
-                    </div>
-                    <div>
-                        <button className='botao-painel'>PAINEL</button>
-                    </div>
+      <section>
+        <div className="fundo">
+          <div className="fundo-cadastrar">
+            <div className="elementos-esquerda">
+              <div className="adicionar-imagens">
+                <input
+                  type="image"
+                  className="campo-imagem"
+                  src="/images/adicionar-imagem.png"
+                  alt="adicionar-imagem"
+                />
+                <div className="adicionar-imagens-dois">
+                  <div>
+                    <input
+                      type="image"
+                      className="campo-imagem-dois"
+                      src="/images/adicionar2.png"
+                      alt="adicionar-imagem"
+                    />
+                    <input
+                      type="image"
+                      className="imagem-dois"
+                      src="/images/adicionar2.png"
+                      alt="adicionar-imagem"
+                    />
+                  </div>
+                  <div>
+                    <input
+                      type="image"
+                      className="campo-imagem-dois"
+                      src="/images/adicionar2.png"
+                      alt="adicionar-imagem"
+                    />
+                    <input
+                      type="image"
+                      className="imagem-dois"
+                      src="/images/adicionar2.png"
+                      alt="adicionar-imagem"
+                    />
+                  </div>
                 </div>
-
-            </header>
-
-            <section>
-
-                <div className='fundo'>
-                    <div className='fundo-cadastrar'>
-                        <div className='elementos-esquerda'>
-                            <div className='adicionar-imagens'>
-                                <input type='image' className='campo-imagem' src='/images/adicionar-imagem.png' alt='adicionar-imagem' />
-                                <div className='adicionar-imagens-dois'>
-                                    <div>
-                                        <input type='image' className='campo-imagem-dois' src='/images/adicionar2.png' alt='adicionar-imagem' />
-                                        <input type='image' className='imagem-dois' src='/images/adicionar2.png' alt='adicionar-imagem' />
-                                    </div>
-                                    <div>
-                                        <input type='image' className='campo-imagem-dois' src='/images/adicionar2.png' alt='adicionar-imagem' />
-                                        <input type='image' className='imagem-dois' src='/images/adicionar2.png' alt='adicionar-imagem' />
-                                    </div>
-                                </div>
-                            </div>
-                            <div className='textarea'>
-                                <p>INFORMAÇÕES DO PRODUTO</p>
-                                <textarea id="story" name="story"
-                                    rows="7" cols="46">
-                                </textarea>
-                            </div>
-                        </div>
-                        <div className='campo-informacao'>
-                            <div>
-                                <div>
-                                    <p>NOME DO PRODUTO</p>
-                                    <input></input>
-                                </div>
-                                <div>
-                                    <p>VALOR</p>
-                                    <input></input>
-                                </div>
-                                <div>
-                                    <p>MARCA</p>
-                                    <input></input>
-                                </div>
-                            </div>
-                            <div>
-                                <div className='cate-tama'>
-                                    <div>
-                                        <p>CATEGORIA</p>
-                                    </div>
-                                    <div>
-                                        <p>TAMANHO</p>
-                                    </div>
-                                </div>
-                                <div>
-                                    <div>
-                                        <p>DISPONÍVEL</p>
-                                        <input type='checkbox' checked=''></input>
-                                    </div>
-                                    <div>
-                                        <p>DESTAQUE</p>
-                                        <input type='checkbox' checked=''></input>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className='botoes'>
-                                <div>
-                                    <button className='botao-excluir'>EXCLUIR PRODUTO</button>
-                                </div>
-                                <div>
-                                    <button className='botao-c-d'>CADASTRA PRODUTO</button>
-                                </div>
-                                <div>
-                                    <button className='botao-c-d'>SALVAR ALTERAÇÃO</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+              </div>
+              <div className="textarea">
+                <p>INFORMAÇÕES DO PRODUTO</p>
+                <textarea id="story" name="story" rows="7" cols="46"></textarea>
+              </div>
+            </div>
+            <div className="campo-informacao">
+              <div className="campo-input">
+                <div>
+                  <p>NOME DO PRODUTO</p>
+                  <input></input>
                 </div>
+                <div className="campo-info">
+                  <p>VALOR</p>
+                  <input></input>
+                </div>
+                <div className="campo-info">
+                  <p>MARCA</p>
+                  <input></input>
+                </div>
+              </div>
+              <div>
+                <div className="cate-tama">
+                  <div className="campo-info">
+                    <p>CATEGORIA</p>
+                    <select>
+                      <option value="" data-default disabled selected></option>
+                      <option value="Acessórios">ACESSÓRIOS</option>
+                      <option value="Masculino">MASCULINO</option>
+                      <option value="Feminino">FEMININO</option>
+                      <option value="Infantil">INFANTIL</option>
+                    </select>
+                  </div>
+                  <div className="campo-info-1">
+                    <p>TAMANHO</p>
+                    <select>
+                      <option value="" data-default disabled selected></option>
+                      <option value="Acessórios">PP</option>
+                      <option value="Acessórios">P</option>
+                      <option value="Masculino">M</option>
+                      <option value="Feminino">G</option>
+                      <option value="Infantil">GG</option>
+                    </select>
+                  </div>
+                </div>
+                <div>
+                  <div className="checkbox">
+                    <p>DISPONÍVEL</p>
+                    <input className="chek" type="checkbox" checked=""></input>
+                  </div>
+                  <div className="checkbox">
+                    <p>DESTAQUE</p>
+                    <input className="chek" type="checkbox" checked=""></input>
+                  </div>
+                </div>
+              </div>
 
-            </section>
-
-        </main>
-    )
+              <div className="botoes">
+          <div>
+            <button className="botao-excluir">EXCLUIR PRODUTO</button>
+          </div>
+          <div>
+            <button className="botao-c-d">CADASTRA PRODUTO</button>
+          </div>
+          <div>
+            <button className="botao-c-d">SALVAR ALTERAÇÃO</button>
+          </div>
+        </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
--- site/src/pages/admin/login/index.js
+++ site/src/pages/admin/login/index.js
@@ -1,50 +1,45 @@
-import './index.scss'
-
-
-
+import "./index.scss";
 
 export default function Login() {
+  return (
+    <main className="page-login">
+      <section className="fundo-tela-login">
+        <div className="bem-vindo-login">
+          <h2 className="bem-vindo-p">
+            BEM-VINDO(a), você está prestes a entrar na área de administradores
+          </h2>
+        </div>
+        <div className="fundo-page-login">
+          <div className="fundo-imagem">
+            <img src="/images/image-login.png" alt="imagem-login"></img>
+          </div>
+          <div className="fundo-login">
+            <div className="info-login">
+              <p>LOGIN</p>
+              <div className="input">
+                <label className="p-input">E-mail</label>
+                <input type="text" id="E-mail"></input>
+              </div>
 
-    return(
-        <main className='page-login'>
+              <div className="input">
+                <label className="p-input">Senha</label>
+                <input type="password" id="Senha"></input>
 
-            <section className='fundo-tela-login'>
-
-                <div className='bem-vindo-login'>
-                    <h2 className='bem-vindo-p'>BEM-VINDO(a), você está prestes a entrar na área de administradores</h2>
+                <div className="olho">
+                  
                 </div>
-                <div className='fundo-page-login'>
-                    <div className='fundo-imagem'>
-                        <img src='/images/image-login.png' alt='imagem-login'></img>
-                    </div>
-                    <div className='fundo-login'>
-                        <div className='info-login'>
-                            <p>LOGIN</p>
-                            <div className='input'>
-                                <p>E-mail</p>
-                                <input type='text' id='E-mail'></input>
-                            </div>
-                            <div className='input'>
-                                <p>Senha</p>
-                                <input type='password' id='Senha'></input>
-                            <div className='olho-senha'>
-                                <img src='/images/olho-senha.png' alt='olho-senha'></img>
-                            </div>
-                            </div>
-                            <button className='botao-entrar'>ENTRAR</button>
-                                <div className='alinhar-linha'>
-                                    
-                                    <p>OU</p>
-                                    
-                                </div>
-                            <button className='botao-cancelar'>CANCELAR</button>
-                            <p className='mensagem-erro'>MENSAGEM DE ERRO</p>
-                        </div>
-                    </div>
-                </div>
+              </div>
 
-            </section>
-
-        </main>
-    )
+              <button className="botao-entrar">ENTRAR</button>
+              <div className="alinhar-linha">
+                <p>------------------ ou ------------------</p>
+              </div>
+              <button className="botao-cancelar">CANCELAR</button>
+              <p className="mensagem-erro">MENSAGEM DE ERRO</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
