2b68833ec185544af7a49c18c0e551feaef82f87
Rayssa Rodrigues <rayssa.rodrigues1198@gmail.com>
2022-10-17 15:14:42 -0300

    .

--- site/src/components/finalizarCompra/index.js
+++ site/src/components/finalizarCompra/index.js
@@ -4,8 +4,37 @@ import '../../common/common.scss'
 
 
 
-export default function FinalizarCompra() {
+export default function FinalizarCompra(props) {
     return (
-        <main id='page-finalizar-compra'></main>
+        <main id='page-finalizar-compra'>
+            <div>
+                <div>
+                    <h6>RESUMO DA COMPRA</h6>
+                    <div className='info-finalizar'>
+                        <div>
+                            <p>Qtd.PRODUTOS:</p>
+                            <p>FRETE:</p>
+                            <p>TOTAL:</p>
+                        </div>
+                        <div>
+                            <p>{props.quantidade}</p>
+                            <p>{props.frete}</p>
+                            <p>{props.total}</p>
+                        </div>
+                    </div>
+                </div>
+                <hr />
+                <div>
+                    <div></div>
+                    <button>CONTINUAR COMPRA</button>
+                    <div className="cartoes">
+                        <img src="/images/cartao-azul.png" alt="cartao-azul" />
+                        <img className="icones" src="/images/cartao-amarelo.png" alt="cartao-amarelo" />
+                        <img className="icones" src="/images/boleto.png" alt="boleto" />
+                        <img className="icones" src="/images/pix.png" alt="pix" />
+                    </div>
+                </div>
+            </div>
+        </main>
     )
 }
--- site/src/pages/user/carrinho/index.js
+++ site/src/pages/user/carrinho/index.js
@@ -19,7 +19,7 @@ export default function Carrinho() {
             <section>
                 <CardCarrinho imagem='/images/produto31.png' nome='Moletom Street Masculino' marca='ADIDAS' preco='R$ 60,00' />
                 <CardCarrinho imagem='/images/produto31.png' nome='Moletom Street Masculino' marca='ADIDAS' preco='R$ 60,00' />
-                <FinalizarCompra />
+                <FinalizarCompra quantidade='2' frete='R$20,00' total='R$ 409,90' />
             </section>
             <section>
                 <Footer />
